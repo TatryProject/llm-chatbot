0594fb8edafde85bfe2326e416e23c0b61b0cdbe
unknown <aagacek2@gmail.com>
2024-05-01 21:24:14 -0500

    Updated settings.py to set Redis host and port based off config file. Removing no-longer-necessary redis_utils.py.

--- llm_chatbot/chatbot_core/views.py
+++ llm_chatbot/chatbot_core/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .tasks import run_prompt_engineering
 import django_rq
+from rq.job import Job
 
 def prompt_engineering(request):
     task = django_rq.enqueue(run_prompt_engineering)
     return JsonResponse({'task_id': task.id})
 
 def get_prompt_engineering_status(request, task_id):
-    print(str(task_id))
-    task = django_rq.fetch_job(str(task_id))
+    task = Job.fetch(str(task_id), django_rq.get_connection())
     if task is None:
         return JsonResponse({'error': 'A job with this ID does not exist.'})
     
